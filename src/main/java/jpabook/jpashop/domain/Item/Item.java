8b4b00662b3c199eab0e20d25d6280bb77f550e0
정상화 <jungsh930210@gmail.com>
2023-03-06 01:45:25 +0900

    edit

--- src/main/java/jpabook/jpashop/domain/Item/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.Item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +13,6 @@ import java.util.List;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 @Getter
-@Setter
 public abstract class Item {
 
     @Id
@@ -26,4 +26,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /**
+     * 비즈니스 로직
+     * => 엔티티 안에서 처리할 수 있는 비즈니스 로직은 엔티티 class 안에서 구현하는 것이 좋음
+     * => 또한, setter의 사용 대신, 아래의 메소드 처럼 접근하는 방법이 더 권장됨 (객체지향적인 방법)
+     */
+    // stock 증가
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    // stock 감소
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
+
 }
