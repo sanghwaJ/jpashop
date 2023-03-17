6cb65686cdbdb2abd0353904b1bae23d1d983700
SanghwaJ <jungsh930210@gmail.com>
2023-03-17 18:22:26 +0900

    edit

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -3,11 +3,13 @@ package jpabook.jpashop.repository.order.query;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(of = "orderId") // OrderQueryDto의 orderId를 기준으로 쿼리를 묶어줌
 public class OrderQueryDto {
     private Long orderId;
     private String name;
@@ -24,4 +26,12 @@ public class OrderQueryDto {
         this.address = address;
     }
 
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDateTime, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDateTime = orderDateTime;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
+    }
 }
