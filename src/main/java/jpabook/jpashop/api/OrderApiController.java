3f7c4e244bb6ccb8e30afce6f1a800fc90f51409
정상화 <jungsh930210@gmail.com>
2023-03-17 22:33:22 +0900

    end

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -10,6 +10,7 @@ import jpabook.jpashop.repository.order.query.OrderFlatDto;
 import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
+import jpabook.jpashop.service.query.OrderQueryService;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -54,13 +55,18 @@ public class OrderApiController {
         return result;
     }
 
+    private final OrderQueryService orderQueryService;
+
     @GetMapping("/api/v3.1/orders")
-    public List<OrderDto> ordersV3() {
-        List<Order> orders = orderRepository.findAllWithItem();
-        List<OrderDto> result = orders.stream()
-                .map(o -> new OrderDto(o))
-                .collect(toList());
-        return result;
+    public List<jpabook.jpashop.service.query.OrderDto> ordersV3() {
+        // List<Order> orders = orderRepository.findAllWithItem();
+        // List<OrderDto> result = orders.stream()
+        //         .map(o -> new OrderDto(o))
+        //         .collect(toList());
+        // return result;
+
+        // OSIV OFF TEST
+        return orderQueryService.ordersV3();
     }
 
     @GetMapping("/api/v3.2/orders")
